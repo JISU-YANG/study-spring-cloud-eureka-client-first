6a30562c4081144c7b9af60a2bcfd58e5d10315b
JISU-YANG <jisuyang@egoist.im>
2024-11-21 17:37:15 +0900

    Config 서버 적용, 설정 덮어쓰기 확인

--- src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
+++ src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
@@ -1,9 +1,11 @@
 package com.spring_cloud.eureka.client.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
