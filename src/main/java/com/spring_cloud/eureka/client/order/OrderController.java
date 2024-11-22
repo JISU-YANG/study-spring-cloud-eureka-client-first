f4d60403d9fa38de377c85ffbbeb2ae5a2c68677
JISU-YANG <jisuyang@egoist.im>
2024-11-22 12:02:44 +0900

    분산 추적 데이터, Zipkin 확인

--- src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
+++ src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
@@ -3,6 +3,7 @@ package com.spring_cloud.eureka.client.order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
@@ -12,8 +13,8 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @GetMapping("/order")
-    public String getOrder() {
-        return "Order Details";
+    @GetMapping("/order/{orderId}")
+    public String getOrder(@PathVariable String orderId) {
+        return orderService.getOrder(orderId);
     }
 }
